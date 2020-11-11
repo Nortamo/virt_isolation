4b95f72cf57066c52b097aca653ffc0501cedabc
Nortamo <40563680+Nortamo@users.noreply.github.com>
2020-11-11 13:00:50 +0200

    A bit more robust

--- laun.c
+++ laun.c
@@ -17,7 +17,6 @@
 #include <signal.h>
 
 
-
 // Wait until child/parent tells that it's ok to proceed.
 void wait_for_action( int * pfd, char * t_buf){
     close(pfd[1]); 
@@ -32,6 +31,26 @@ void tell_done(int *pfd){
     close(pfd[1]);               
 }
 
+void get_exit_status(int *pfd,const char * cmd){
+        int set_stat;
+        close(pfd[1]);
+        read(pfd[0],&set_stat,sizeof(int));
+        if(set_stat!=0){
+            fprintf(stderr, "The command %s has failed.\n", cmd); 
+            exit(EXIT_FAILURE);    
+        }
+
+}
+
+void send_exit_status(int *pfd, int status){
+        int buf_s;
+        buf_s=WEXITSTATUS(status);
+        close(pfd[0]);              
+        write(pfd[1],&buf_s, sizeof(int));
+}
+
+
+
 // Check return value and exit on failure
 void check_ret(int ret_val, const char * fun){
     if(ret_val == -1){
@@ -39,11 +58,21 @@ void check_ret(int ret_val, const char * fun){
         exit(EXIT_FAILURE);
     }
 }
+void check_ret_kill(int ret_val, const char * fun, pid_t parent_pid){
+    if(ret_val != 0){
+        fprintf(stderr, "The call to %s has failed.\n", fun); 
+        fprintf(stderr, "Killing main process.\n"); 
+        kill(parent_pid,9);
+        exit(EXIT_FAILURE);
+    }
+
+}
 
 
 int main(int argc, char *argv[]) {
     // Used for blocking
     int pipefd[2];
+    int msg_pipe[2];
     char buf;
 
     // The starting uid we wish to map back to.
@@ -53,19 +82,22 @@ int main(int argc, char *argv[]) {
     int p_pid=getpid();
 
 
-
     pid_t c1_pid;
     check_ret(pipe(pipefd),"pipe()");       
+    check_ret(pipe(msg_pipe),"pipe()");        
     c1_pid = fork();                 
     check_ret(c1_pid,"fork()");
 
     // First child process
     if (c1_pid == 0){
+        // Terminate if the parent exits
+        prctl(PR_SET_PDEATHSIG, SIGHUP);
         // Set mapping and tell the parent that we are done
         // gids are mapped 1 to 1
         // map starting uid to 0 and the rest 1 to 1
         sprintf(cmd,"./set %d %d 0",o_uid,p_pid);
-        system(cmd);
+        int status=system(cmd);
+        send_exit_status(msg_pipe,status); 
         tell_done(pipefd); 
         _exit(EXIT_SUCCESS);
 
@@ -77,9 +109,8 @@ int main(int argc, char *argv[]) {
         unshare(CLONE_NEWNS|CLONE_NEWUSER);
         // Wait for the child to do the id mapping
         wait_for_action(pipefd,&buf);
-
+        get_exit_status(msg_pipe,"./set");
         pid_t c2_pid;
-        pid_t c_p_pid=getpid();
 
         c2_pid = fork();                   
         check_ret(c2_pid,"fork()");
@@ -88,7 +119,8 @@ int main(int argc, char *argv[]) {
             // Terminate if the parent exits
             prctl(PR_SET_PDEATHSIG, SIGHUP);
             // Mount a squashfs file image
-            check_ret(
+            // Kill the parent process if we fail
+            check_ret_kill(
                     execl("/usr/bin/squashfuse",
                         "/usr/bin/squashfuse",
                         "-f",
@@ -96,22 +128,26 @@ int main(int argc, char *argv[]) {
                         "/mnt" ,
                         (char*) NULL
                         )
-                    ,"execl()");
+                    ,"execl()",p_pid);
         } 
         // main process
         else{   
             pid_t c3_pid;
             check_ret(pipe(pipefd),"pipe()");      
+            check_ret(pipe(msg_pipe),"pipe()");        
             c3_pid = fork();                   
             check_ret(c3_pid,"fork()");
             // Third child process
             if (c3_pid == 0){
+                // Terminate if the parent exits
+                prctl(PR_SET_PDEATHSIG, SIGHUP);
                 // Set mapping and tell the parent that we are done
                 // gids are mapped 1 to 1
                 // uid mapping is reversed so that we are back to our
                 // original uid
-                sprintf(cmd,"./set %d %d 1",o_uid,c_p_pid);
-                system(cmd);
+                sprintf(cmd,"./set %d %d 1",o_uid,p_pid);
+                int status=system(cmd);
+                send_exit_status(msg_pipe,status); 
                 tell_done(pipefd);
                 _exit(EXIT_SUCCESS);
             }
@@ -120,6 +156,7 @@ int main(int argc, char *argv[]) {
                 // revert back to our original uid 
                 unshare(CLONE_NEWUSER); 
                 wait_for_action(pipefd,&buf);
+                get_exit_status(msg_pipe,"./set");
                 execvp(argv[1],argv+1);
             }
 
--- set.c
+++ set.c
@@ -15,55 +15,57 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 
+
+void check_file_ret(int ret_val,const char * action,const char * file_name){
+    if(ret_val == -1){
+        fprintf(stderr, "Failed to %s file: %s !\n", action,file_name); 
+        exit(EXIT_FAILURE);
+    }
+} 
+
 int main(int argc, char *argv[]) {
     int fd;
     int uid= atoi(argv[1]);
     int pid=atoi(argv[2]);
-    int opt=atoi(argv[3]);
+    int mode=atoi(argv[3]);
     
-
-
-    char * gid_map;
+    const char * gid_map;
     // Group mappings are not changed in the new namespace
     gid_map="0 0 4294967295";
     size_t gid_len=strlen(gid_map);
     char uid_map[100];
-    if(opt==0){
+    if(mode==0){
     sprintf(uid_map,"0 %d 1\n%d %d %d\n%d %d %d",uid,1,1,uid-1,uid+1,uid+1,20000000);
     }
-    else if(opt==1){
+    else if(mode==1){
     sprintf(uid_map,"%d 0 1\n%d %d %d\n%d %d %d",uid,1,1,uid-1,uid+1,uid+1,20000000);
     }
+    else{
+        fprintf(stderr,"Unknown mode %d.\n",mode);
+        return -1;
+    }
     size_t uid_len=strlen(uid_map);
-    char g_f[100];
-    char id_f[100];
-    char gi_f[100];
+    char setgroups_f[100];
+    char uid_map_f[100];
+    char gid_map_f[100];
     int res;
-    sprintf(g_f,"/proc/%d/setgroups",pid);
-    sprintf(id_f,"/proc/%d/uid_map",pid);
-    sprintf(gi_f,"/proc/%d/gid_map",pid);
-        printf("Child file: %s\n ",g_f);  
-        printf("Child file: %s\n ",id_f);  
-        fd=open(g_f,O_WRONLY);
-        if(fd==-1){
-            printf("Failed to open file!:\n");
-        }
-        res=write(fd,"deny",4);
-        if(res == -1){
-            printf("Failed to write deny!\n");
-        }
-        close(fd);                             
-        fd=open(id_f,O_WRONLY);
-        res=write(fd,uid_map,uid_len);          
-        if(res == -1){                            
-            printf("Writing to uid map failed\n");
-        }                                         
-        close(fd);                             
-        fd=open(gi_f,O_WRONLY);
-        res=write(fd,gid_map,gid_len);          
-        if(res == -1){                            
-            printf("Writing to gid map failed\n");
-        }                                         
-
-    
+    sprintf(setgroups_f,"/proc/%d/setgroups",pid);
+    sprintf(uid_map_f,"/proc/%d/uid_map",pid);
+    sprintf(gid_map_f,"/proc/%d/gid_map",pid);
+    fd=open(setgroups_f,O_WRONLY);
+    check_file_ret(fd,"open",setgroups_f);
+    res=write(fd,"deny",4);
+    check_file_ret(res,"write to",setgroups_f);
+    close(fd);                             
+    fd=open(uid_map_f,O_WRONLY);
+    check_file_ret(fd,"open",uid_map_f);
+    res=write(fd,uid_map,uid_len);          
+    check_file_ret(res,"write to",uid_map_f);
+    close(fd);                             
+    fd=open(gid_map_f,O_WRONLY);
+    check_file_ret(fd,"open",gid_map_f);
+    res=write(fd,gid_map,gid_len); 
+    check_file_ret(res,"write to",gid_map_f);
+    close(fd);
+    return 0;    
 }
